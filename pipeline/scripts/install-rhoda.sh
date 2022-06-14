2bf31e3d506121d8df1aa313bf583eaf415ea662
veniceofcode <rbanda@redhat.com>
2022-06-14 22:14:37 +0100

    cleanup

--- pipeline/scripts/install-rhoda.sh
+++ pipeline/scripts/install-rhoda.sh
@@ -44,21 +44,19 @@ spec:
   sourceNamespace: openshift-marketplace
   startingCSV: dbaas-operator.v0.1.5
 EOF
-echo "Check if RHODA Operator pod is ready"
-
-for i in {1..150}; do  # timeout after 5 minutes
-  pods="$(oc get pods -n openshift-dbaas-operator --no-headers -l name=dbaas-operator 2>/dev/null |grep dbaas-operator | wc -l)"
-  if [[ "${pods}" -ge 1 ]]; then
-    echo -e "\nWaiting for RHODA operator pod"
-    oc wait --for=condition=Ready -n openshift-dbaas-operator -l name=dbaas-operator pod --timeout=5m
-    retval=$?
-    if [[ "${retval}" -gt 0 ]]; then exit "${retval}"; else break; fi
-  fi
-  if [[ "${i}" -eq 150 ]]; then
-    echo "Timeout: pod was not created."
-    exit 2
-  fi
-  echo -n "."
-  sleep 10
-done
-
+#echo "Check if RHODA Operator pod is ready"
+#for i in {1..150}; do  # timeout after 5 minutes
+#  pods="$(oc get pods -n openshift-dbaas-operator --no-headers -l name=dbaas-operator 2>/dev/null |grep dbaas-operator | wc -l)"
+#  if [[ "${pods}" -ge 1 ]]; then
+#    echo -e "\nWaiting for RHODA operator pod"
+#    oc wait --for=condition=Ready -n openshift-dbaas-operator -l name=dbaas-operator pod --timeout=5m
+#    retval=$?
+#    if [[ "${retval}" -gt 0 ]]; then exit "${retval}"; else break; fi
+#  fi
+#  if [[ "${i}" -eq 150 ]]; then
+#    echo "Timeout: pod was not created."
+#    exit 2
+#  fi
+#  echo -n "."
+#  sleep 10
+#done
