8fefe5b2bab88605086624b44ddc30fc280378ef
veniceofcode <rbanda@redhat.com>
2022-06-14 14:02:00 +0100

    Update install-rhoda.sh

--- pipeline/scripts/install-rhoda.sh
+++ pipeline/scripts/install-rhoda.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 echo "Installing RHODA operator"
 CHANNEL=${1:-alpha}
 CATALOG_SOURCE=${2:-openshift-marketplace}
@@ -57,5 +56,3 @@ for i in {1..150}; do  # timeout after 5 minutes
   echo -n "."
   sleep 2
 done
-echo "Openshift Database Access Operator is installed and Running!"
-`EOF'
