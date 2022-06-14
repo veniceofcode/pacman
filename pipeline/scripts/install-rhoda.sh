1e3f38e8da8d0a7d73f01bc35e8dd333a4c6466a
veniceofcode <rbanda@redhat.com>
2022-06-14 14:37:45 +0100

    Update install-rhoda.sh

--- pipeline/scripts/install-rhoda.sh
+++ pipeline/scripts/install-rhoda.sh
@@ -2,43 +2,7 @@
 echo "Installing RHODA operator"
 CHANNEL=${1:-alpha}
 CATALOG_SOURCE=${2:-openshift-marketplace}
-cat <<EOF | oc apply -f -
----
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: openshift-dbaas-operator
----
-apiVersion: operators.coreos.com/v1alpha1
-kind: CatalogSource
-metadata:
-  name: dbaas-operator
-  namespace: openshift-marketplace
-spec:
-  sourceType: grpc
-  image: quay.io/osd-addons/dbaas-operator-index@sha256:feac28aae2c33fa77122c1a0663a258b851db83beb2c33a281d6b50eab8b96e4
-  displayName: DBaaS Operator
----
-apiVersion: operators.coreos.com/v1
-kind: OperatorGroup
-metadata:
-  name: dbaas-operator
-  namespace: openshift-dbaas-operator
----
-apiVersion: operators.coreos.com/v1alpha1
-kind: Subscription
-metadata:
-  name: dbaas-operator
-  namespace: openshift-dbaas-operator
-spec:
-  source: dbaas-operator
-  sourceNamespace: openshift-marketplace
-  name: dbaas-operator
-  installPlanApproval: Automatic
-  channel: $CHANNEL
-  startingCSV: dbaas-operator.v0.1.5
-EOF
-
+oc process -f https://raw.githubusercontent.com/veniceofcode/pacman/master/pipeline/scripts/install.yaml | oc apply -f-
 sleep 2
 echo "Check if RHODA Operator pod is ready"
 for i in {1..150}; do  # timeout after 5 minutes
