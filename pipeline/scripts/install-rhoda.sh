2e49940a3f9f69dd302763269a6f73849599df59
veniceofcode <rbanda@redhat.com>
2022-06-14 16:51:09 +0100

    Added pipeline task for rhoda

--- pipeline/scripts/install-rhoda.sh
+++ pipeline/scripts/install-rhoda.sh
@@ -14,8 +14,47 @@ spec:
   image: quay.io/osd-addons/dbaas-operator-index@sha256:feac28aae2c33fa77122c1a0663a258b851db83beb2c33a281d6b50eab8b96e4
   displayName: DBaaS Operator
 EOF
-
-oc process -f https://raw.githubusercontent.com/veniceofcode/pacman/master/pipeline/scripts/install.yaml | oc apply -f-
+cat <<EOF | oc apply -f -
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: openshift-dbaas-operator
+EOF
+cat <<EOF | oc apply -f -
+apiVersion: operators.coreos.com/v1alpha1
+kind: CatalogSource
+metadata:
+ name: dbaas-operator
+ namespace: openshift-marketplace
+spec:
+  sourceType: grpc
+  image: quay.io/osd-addons/dbaas-operator-index@sha256:feac28aae2c33fa77122c1a0663a258b851db83beb2c33a281d6b50eab8b96e4
+  displayName: DBaaS Operator
+EOF
+cat <<EOF | oc apply -f -
+apiVersion: operators.coreos.com/v1
+kind: OperatorGroup
+metadata:
+  name: dbaas-operator
+  namespace: openshift-dbaas-operator
+spec:
+EOF
+cat <<EOF | oc apply -f -
+apiVersion: operators.coreos.com/v1alpha1
+kind: Subscription
+metadata:
+  labels:
+    operators.coreos.com/dbaas-operator.openshift-dbaas-operator: ""
+  name: dbaas-operator
+  namespace: openshift-dbaas-operator
+spec:
+  channel: alpha
+  installPlanApproval: Automatic
+  name: dbaas-operator
+  source: dbaas-operator
+  sourceNamespace: openshift-marketplace
+  startingCSV: dbaas-operator.v0.1.5
+EOF
 
 echo "Check if RHODA Operator pod is ready"
 
@@ -31,6 +70,5 @@ for i in {1..150}; do  # timeout after 5 minutes
     echo "Timeout: pod was not created."
     exit 2
   fi
-  echo -n "Completed installation of Red Hat OpenShift Database Access Operator"
+  echo -n "."
 done
-
